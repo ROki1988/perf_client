3d3cd25b72f544f44552c2fd17c5d5badd2a7b3e
ROki1988 <roki.swindler@gmail.com>
2016-10-25 23:21:32 +0900

    impl ToString for PdhValue

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,29 @@ extern crate winapi;
 
 mod pdh_wrapper;
 
+use pdh_wrapper::*;
+
 fn main() {
 
-    let element_list = vec![pdh_wrapper::PdhCounterPathElement::new(String::from("Memory"),
-                                                                    String::from("Available \
-                                                                                  Mbytes"))];
+    let element_list = vec![PdhCounterPathElement::new(String::from("Memory"),
+                                                       String::from("Available Mbytes"))];
 
     let path_list = element_list.into_iter()
-        .filter_map(|e| pdh_wrapper::pdh_make_counter_path(&e).ok())
+        .filter_map(|e| pdh_make_counter_path(&e).ok())
         .collect::<Vec<_>>();
 
-    let pdhc = pdh_wrapper::PdhController::new(&path_list).expect("Can't create Metrics Collector");
+    let pdhc = PdhController::new(&path_list).expect("Can't create Metrics Collector");
     println!("{:?}",
-             pdhc.into_iter().map(|v| pdh_value2str(v)).collect::<Vec<_>>());
+             pdhc.into_iter().map(|v| v.to_string()).collect::<Vec<_>>());
 }
 
-fn pdh_value2str(v: pdh_wrapper::PdhValue) -> String {
-
-    match v {
-        pdh_wrapper::PdhValue::LongLong(ll) => format!("{:.3}", ll),
-        pdh_wrapper::PdhValue::Long(l) => format!("{:.3}", l),
-        pdh_wrapper::PdhValue::Double(d) => format!("{:.3}", d),
-        pdh_wrapper::PdhValue::Str(s) => s,
+impl ToString for PdhValue {
+    fn to_string(&self) -> String {
+        match *self {
+            PdhValue::LongLong(ref ll) => format!("{:.3}", ll),
+            PdhValue::Long(ref l) => format!("{:.3}", l),
+            PdhValue::Double(ref d) => format!("{:.3}", d),
+            PdhValue::Str(ref s) => s.clone(),
+        }
     }
 }
